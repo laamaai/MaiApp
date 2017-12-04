da5e5daafb2bbb334b3eba645f5e8a1ebe2b5e1d
mikel <lopezmikel738@gmail.com>
2017-12-04 18:00:45 +0100

    Sacar mensajes de error personalizados, queda probar si funciona correctamente

--- app/src/main/java/com/example/maider/maiapp/Cinco.java
+++ app/src/main/java/com/example/maider/maiapp/Cinco.java
@@ -12,13 +12,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class Cinco extends Fragment {
     private static final String TAG = "FragmentCinco";
     TextView scrollable;
     private EditText rp1, rp2;
+    private Button btnAceptar;
 
     @SuppressLint("ResourceAsColor")
     @Override
@@ -32,9 +35,22 @@ public class Cinco extends Fragment {
         scrollable = (TextView) view.findViewById(R.id.txtAcertijo1);
         scrollable.setMovementMethod(new ScrollingMovementMethod());
         title.setTypeface(font);
-        Button btn = (Button)view.findViewById(R.id.button2);
+
         rp1 = (EditText)view.findViewById(R.id.primera);
         rp2 = (EditText)view.findViewById(R.id.segunda);
+        btnAceptar = (Button) view.findViewById(R.id.btnAceptar);
+
+        //Mostramos el dialog para ver si ha acertado o no
+        btnAceptar.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+               corregir(Cinco.super.getView());
+            }
+        });
+
+
+
 
         // Calligrapher cali = new Calligrapher(getActivity());
         // cali.setFont(getActivity(),"font/Londrina.ttf",true);
@@ -47,13 +63,54 @@ public class Cinco extends Fragment {
         String r2 = rp2.getText().toString().toUpperCase();
         String resp = getString(R.string.acertijoRespuesta1);
         String ellos = "" +r1+"-"+r2;
+    }
 
+    public void SacarMensajeExito(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(Cinco.super.getActivity());
 
+        View view = LayoutInflater.from(Cinco.super.getActivity()).inflate(R.layout.custom_layout, null);
 
+        TextView title = (TextView) view.findViewById(R.id.title);
+        ImageButton imageButton = (ImageButton) view.findViewById(R.id.image);
 
+        title.setText("Hello There!");
+
+        imageButton.setImageResource(R.drawable.iconmap);
+
+        builder.setPositiveButton("Chii", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(Cinco.super.getActivity(), "Has acertado", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        builder.setView(view);
+        builder.show();
     }
 
 
+    public void SacarMensajeError(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(Cinco.super.getActivity());
+
+        View view = LayoutInflater.from(Cinco.super.getActivity()).inflate(R.layout.custom_layout, null);
+
+        TextView title = (TextView) view.findViewById(R.id.title);
+        ImageButton imageButton = (ImageButton) view.findViewById(R.id.image);
+
+        title.setText("Hello There!");
+
+        imageButton.setImageResource(R.drawable.iconmap);
+
+        builder.setPositiveButton("Chii", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(Cinco.super.getActivity(), "Has fallado, vuelve a intentarlo", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        builder.setView(view);
+        builder.show();
+    }
 
 
 
